6462da58673623228e8f1c15f21714e4c019c524
Brokkonaut <hannos17@gmx.de>
2020-11-04 05:14:46 +0100

    Create CubesideNMSUtils plugin

---
+++ nmsutils-plugin/src/main/java/de/cubeside/nmsutils/NMSUtilsPlugin.java
@@ -0,0 +1,18 @@
+package de.cubeside.nmsutils;
+
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NMSUtilsPlugin extends JavaPlugin {
+    private NMSUtils nmsUtils;
+
+    @Override
+    public void onEnable() {
+        nmsUtils = NMSUtils.createInstance(this);
+        getServer().getServicesManager().register(NMSUtils.class, nmsUtils, this, ServicePriority.Normal);
+    }
+
+    public NMSUtils getNmsUtils() {
+        return nmsUtils;
+    }
+}
