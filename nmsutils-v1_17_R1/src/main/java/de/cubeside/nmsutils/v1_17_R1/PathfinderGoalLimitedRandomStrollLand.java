181dfba5c35fcabbf835cf3294165adcc8b244db
Brokkonaut <hannos17@gmx.de>
2021-06-20 04:45:38 +0200

    Fix pathfinder for 1.17

--- nmsutils-v1_17_R1/src/main/java/de/cubeside/nmsutils/v1_17_R1/PathfinderGoalLimitedRandomStrollLand.java
+++ nmsutils-v1_17_R1/src/main/java/de/cubeside/nmsutils/v1_17_R1/PathfinderGoalLimitedRandomStrollLand.java
@@ -4,7 +4,6 @@ import java.util.function.Function;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.phys.Vec3;
-import org.bukkit.craftbukkit.util.CraftVector;
 import org.bukkit.util.Vector;
 
 public class PathfinderGoalLimitedRandomStrollLand extends RandomStrollGoal {
@@ -21,7 +20,7 @@ public class PathfinderGoalLimitedRandomStrollLand extends RandomStrollGoal {
     @Override
     protected Vec3 getPosition() {
         Vec3 base = super.getPosition();
-        if (base == null || checkVectorFunction.apply(CraftVector.toBukkit(base)) != Boolean.TRUE) {
+        if (base == null || checkVectorFunction.apply(org.bukkit.craftbukkit.v1_17_R1.util.CraftVector.toBukkit(base)) != Boolean.TRUE) {
             base = null;
         }
         return base;
