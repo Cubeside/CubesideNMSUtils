ef5ed7892634da23b8eb7333c40e5b3b22951f7c
Brokkonaut <hannos17@gmx.de>
2024-04-13 10:04:11 +0200

    cleanup & eclipse compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,3 @@
 plugins {
-    `maven-publish`
-    `java-library`
     id("io.papermc.paperweight.userdev") version "1.5.13" apply false
-}
-
-group = "de.cubeside.nmsutils"
-version = "0.0.1-SNAPSHOT"
-description = "nms adapter"
-
-repositories {
-    mavenCentral()
-    mavenLocal()
-}
-
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
-}
-
-dependencies {
-
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "nmsutils_parent"
+// rootProject.name = "nmsutils_parent"
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
@@ -7,7 +7,8 @@ include("core")
 // Include all adapters
 val adapterDirName = "adapter";
 // Get all directories inside the adapters directory
-val subprojectDirs = File(adapterDirName).listFiles().filter { it.isDirectory }
+
+val subprojectDirs = File(settings.settingsDir, adapterDirName).listFiles().filter { it.isDirectory && !it.name.startsWith(".")}
 // Add them as subprojects
 subprojectDirs.forEach { subprojectDir ->
     include(adapterDirName + ":" + subprojectDir.name)
