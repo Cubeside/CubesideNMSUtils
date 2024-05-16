9fc5a7048af0bc0d58cdd4cbdae7a724b4482c49
Brokkonaut <hannos17@gmx.de>
2024-05-16 04:45:55 +0200

    Add data updater

--- adapter/paper1_20_6/src/main/java/de/cubeside/nmsutils/paper1_20_6/NbtUtilsImpl.java
+++ adapter/paper1_20_6/src/main/java/de/cubeside/nmsutils/paper1_20_6/NbtUtilsImpl.java
@@ -1,5 +1,7 @@
 package de.cubeside.nmsutils.paper1_20_6;
 
+import com.mojang.datafixers.DSL;
+import com.mojang.serialization.Dynamic;
 import de.cubeside.nmsutils.NbtUtils;
 import de.cubeside.nmsutils.nbt.CompoundTag;
 import java.io.ByteArrayInputStream;
@@ -7,6 +9,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import net.minecraft.SharedConstants;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.util.datafix.DataFixers;
+import net.minecraft.util.datafix.fixes.References;
 
 public class NbtUtilsImpl implements NbtUtils {
     // private final NMSUtilsImpl nmsUtils;
@@ -47,5 +55,55 @@ public class NbtUtilsImpl implements NbtUtils {
     @Override
     public String writeString(CompoundTag in) {
         return net.minecraft.nbt.NbtUtils.structureToSnbt(((CompoundTagImpl) in).handle);
+    }
+
+    @Override
+    public int getCurrentDataVersion() {
+        return SharedConstants.getCurrentVersion().getDataVersion().getVersion();
+    }
+
+    private CompoundTag updateNbt(DSL.TypeReference type, CompoundTag in, int oldVersion) {
+        int currentVersion = getCurrentDataVersion();
+        if (oldVersion >= currentVersion || in == null) {
+            return in;
+        }
+        Dynamic<Tag> dyn = new Dynamic<>(NbtOps.INSTANCE, ((CompoundTagImpl) in).handle);
+        Tag out = DataFixers.getDataFixer().update(type, dyn, oldVersion, currentVersion).getValue();
+        if (out instanceof net.minecraft.nbt.CompoundTag compound) {
+            return new CompoundTagImpl(compound);
+        }
+        throw new RuntimeException("Expected CompoundTag as result of update, but got " + out.getClass().getName() + " !");
+    }
+
+    private String updateName(DSL.TypeReference type, String in, int oldVersion) {
+        int currentVersion = getCurrentDataVersion();
+        if (oldVersion >= currentVersion || in == null) {
+            return in;
+        }
+        Tag out = DataFixers.getDataFixer().update(type, new Dynamic<>(NbtOps.INSTANCE, StringTag.valueOf(in)), oldVersion, currentVersion).getValue();
+        if (out instanceof net.minecraft.nbt.StringTag stringTag) {
+            return stringTag.getAsString();
+        }
+        throw new RuntimeException("Expected StringTag as result of update, but got " + out.getClass().getName() + " !");
+    }
+
+    @Override
+    public CompoundTag updateEntity(CompoundTag in, int oldVersion) {
+        return updateNbt(References.ENTITY, in, oldVersion);
+    }
+
+    @Override
+    public CompoundTag updateBlockState(CompoundTag in, int oldVersion) {
+        return updateNbt(References.BLOCK_ENTITY, in, oldVersion);
+    }
+
+    @Override
+    public String updateItemTypeName(String in, int oldVersion) {
+        return updateName(References.ITEM_NAME, in, oldVersion);
+    }
+
+    @Override
+    public String updateBlockTypeName(String in, int oldVersion) {
+        return updateName(References.BLOCK_NAME, in, oldVersion);
     }
 }
--- core/src/main/java/de/cubeside/nmsutils/NbtUtils.java
+++ core/src/main/java/de/cubeside/nmsutils/NbtUtils.java
@@ -1,6 +1,7 @@
 package de.cubeside.nmsutils;
 
 import de.cubeside.nmsutils.nbt.CompoundTag;
+import org.bukkit.Bukkit;
 
 public interface NbtUtils {
     public CompoundTag parseBinary(byte[] in);
@@ -10,4 +11,25 @@ public interface NbtUtils {
     public CompoundTag parseString(String in);
 
     public String writeString(CompoundTag in);
+
+    @SuppressWarnings("deprecation")
+    public default int getCurrentDataVersion() {
+        return Bukkit.getUnsafe().getDataVersion();
+    }
+
+    public default CompoundTag updateEntity(CompoundTag in, int oldDataVersion) {
+        return in;
+    }
+
+    public default CompoundTag updateBlockState(CompoundTag in, int oldDataVersion) {
+        return in;
+    }
+
+    public default String updateItemTypeName(String in, int oldDataVersion) {
+        return in;
+    }
+
+    public default String updateBlockTypeName(String in, int oldDataVersion) {
+        return in;
+    }
 }
