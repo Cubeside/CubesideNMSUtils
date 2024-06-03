948799e7a115c53846d213d0c6f665ec51726432
Brokkonaut <hannos17@gmx.de>
2024-06-03 21:23:00 +0200

    add getVaultRewardedPlayers

--- adapter/paper1_20_6/src/main/java/de/cubeside/nmsutils/paper1_20_6/BlockUtilsImpl.java
+++ adapter/paper1_20_6/src/main/java/de/cubeside/nmsutils/paper1_20_6/BlockUtilsImpl.java
@@ -2,7 +2,14 @@ package de.cubeside.nmsutils.paper1_20_6;
 
 import de.cubeside.nmsutils.BlockUtils;
 import de.cubeside.nmsutils.NMSUtils;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.vault.VaultBlockEntity;
+import net.minecraft.world.level.block.entity.vault.VaultServerData;
 import net.minecraft.world.phys.BlockHitResult;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.CraftWorld;
@@ -12,6 +19,8 @@ import org.bukkit.entity.Player;
 
 public class BlockUtilsImpl implements BlockUtils {
     private final NMSUtilsImpl nmsUtils;
+
+    private Field vaultRewardedPlayersField;
 
     public BlockUtilsImpl(NMSUtilsImpl nmsUtils) {
         this.nmsUtils = nmsUtils;
@@ -29,5 +38,35 @@ public class BlockUtilsImpl implements BlockUtils {
 
         BlockHitResult blockHitResult = new BlockHitResult(craftBlock.getPosition().getCenter(), Direction.UP, craftBlock.getPosition(), false);
         return craftBlock.getNMS().useWithoutItem(world.getHandle(), ((CraftPlayer) player).getHandle(), blockHitResult).consumesAction();
+    }
+
+    @Override
+    public Set<UUID> getVaultRewardedPlayers(Block block) {
+        CraftBlock craftBlock = ((CraftBlock) block);
+        CraftWorld world = ((CraftWorld) block.getWorld());
+
+        BlockEntity blockEntity = world.getHandle().getBlockEntity(craftBlock.getPosition());
+        if (blockEntity instanceof VaultBlockEntity vaultBlock) {
+            VaultServerData serverData = vaultBlock.getServerData();
+            if (vaultRewardedPlayersField == null) {
+                for (Field field : serverData.getClass().getDeclaredFields()) {
+                    if (Set.class.isAssignableFrom(field.getType())) {
+                        vaultRewardedPlayersField = field;
+                        field.setAccessible(true);
+                    }
+                }
+                if (vaultRewardedPlayersField == null) {
+                    throw new IllegalStateException("vaultRewardedPlayersField not found!");
+                }
+            }
+            try {
+                @SuppressWarnings("unchecked")
+                Set<UUID> playersSet = (Set<UUID>) vaultRewardedPlayersField.get(serverData);
+                return Collections.unmodifiableSet(playersSet);
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                throw new RuntimeException("Could not access vault rewardedPlayers field", e);
+            }
+        }
+        throw new IllegalArgumentException("This block is not a vault");
     }
 }
--- core/src/main/java/de/cubeside/nmsutils/BlockUtils.java
+++ core/src/main/java/de/cubeside/nmsutils/BlockUtils.java
@@ -1,5 +1,7 @@
 package de.cubeside.nmsutils;
 
+import java.util.Set;
+import java.util.UUID;
 import java.util.logging.Level;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -7,8 +9,30 @@ import org.bukkit.entity.Player;
 public interface BlockUtils {
     public NMSUtils getNMSUtils();
 
+    /**
+     * Fakes a use of a block by a player.
+     *
+     * @param block
+     *            the block to use
+     * @param player
+     *            the player who uses the block
+     * @return true if the block could be used
+     */
     public default boolean useBlock(Block block, Player player) {
         getNMSUtils().getPlugin().getLogger().log(Level.SEVERE, "Call to unimplemented method", new RuntimeException());
         return false;
+    }
+
+    /**
+     * Returns a set of players who previously accessed a vault. The block must be a vault block.
+     * The returned set is backed by the internal set, so it should be copied if you want to store it.
+     *
+     * @param block
+     *            a block that contains a vault
+     * @return a set of players who have accessed this vault
+     */
+    public default Set<UUID> getVaultRewardedPlayers(Block block) {
+        getNMSUtils().getPlugin().getLogger().log(Level.SEVERE, "Call to unimplemented method", new RuntimeException());
+        return Set.of();
     }
 }
