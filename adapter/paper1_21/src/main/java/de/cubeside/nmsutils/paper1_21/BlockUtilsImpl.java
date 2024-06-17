d44705bf94d9ce7e664d97f94aab994f091b95e8
Brokkonaut <hannos17@gmx.de>
2024-06-17 21:35:42 +0200

    Start update for 1.21

---
+++ adapter/paper1_21/src/main/java/de/cubeside/nmsutils/paper1_21/BlockUtilsImpl.java
@@ -0,0 +1,72 @@
+package de.cubeside.nmsutils.paper1_21;
+
+import de.cubeside.nmsutils.BlockUtils;
+import de.cubeside.nmsutils.NMSUtils;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.vault.VaultBlockEntity;
+import net.minecraft.world.level.block.entity.vault.VaultServerData;
+import net.minecraft.world.phys.BlockHitResult;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.block.CraftBlock;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class BlockUtilsImpl implements BlockUtils {
+    private final NMSUtilsImpl nmsUtils;
+
+    private Field vaultRewardedPlayersField;
+
+    public BlockUtilsImpl(NMSUtilsImpl nmsUtils) {
+        this.nmsUtils = nmsUtils;
+    }
+
+    @Override
+    public NMSUtils getNMSUtils() {
+        return nmsUtils;
+    }
+
+    @Override
+    public boolean useBlock(Block block, Player player) {
+        CraftBlock craftBlock = ((CraftBlock) block);
+        CraftWorld world = ((CraftWorld) block.getWorld());
+
+        BlockHitResult blockHitResult = new BlockHitResult(craftBlock.getPosition().getCenter(), Direction.UP, craftBlock.getPosition(), false);
+        return craftBlock.getNMS().useWithoutItem(world.getHandle(), ((CraftPlayer) player).getHandle(), blockHitResult).consumesAction();
+    }
+
+    @Override
+    public Set<UUID> getVaultRewardedPlayers(Block block) {
+        CraftBlock craftBlock = ((CraftBlock) block);
+        CraftWorld world = ((CraftWorld) block.getWorld());
+
+        BlockEntity blockEntity = world.getHandle().getBlockEntity(craftBlock.getPosition());
+        if (blockEntity instanceof VaultBlockEntity vaultBlock) {
+            VaultServerData serverData = vaultBlock.getServerData();
+            if (vaultRewardedPlayersField == null) {
+                for (Field field : serverData.getClass().getDeclaredFields()) {
+                    if (Set.class.isAssignableFrom(field.getType())) {
+                        vaultRewardedPlayersField = field;
+                        field.setAccessible(true);
+                    }
+                }
+                if (vaultRewardedPlayersField == null) {
+                    throw new IllegalStateException("vaultRewardedPlayersField not found!");
+                }
+            }
+            try {
+                @SuppressWarnings("unchecked")
+                Set<UUID> playersSet = (Set<UUID>) vaultRewardedPlayersField.get(serverData);
+                return Collections.unmodifiableSet(playersSet);
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                throw new RuntimeException("Could not access vault rewardedPlayers field", e);
+            }
+        }
+        throw new IllegalArgumentException("This block is not a vault");
+    }
+}
---
+++ adapter/paper1_21/src/main/java/de/cubeside/nmsutils/paper1_21/PathfinderGoalLimitedRandomStrollLand.java
@@ -0,0 +1,29 @@
+package de.cubeside.nmsutils.paper1_21;
+
+import java.util.function.Function;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.phys.Vec3;
+import org.bukkit.craftbukkit.util.CraftVector;
+import org.bukkit.util.Vector;
+
+public class PathfinderGoalLimitedRandomStrollLand extends RandomStrollGoal {
+
+    private Function<Vector, Boolean> checkVectorFunction;
+    // private EntityCreature entity;
+
+    public PathfinderGoalLimitedRandomStrollLand(PathfinderMob entity, double velocity, Function<Vector, Boolean> checkVectorFunction) {
+        super(entity, velocity, 1, false);
+        // this.entity = entity;
+        this.checkVectorFunction = checkVectorFunction;
+    }
+
+    @Override
+    protected Vec3 getPosition() {
+        Vec3 base = super.getPosition();
+        if (base == null || checkVectorFunction.apply(CraftVector.toBukkit(base)) != Boolean.TRUE) {
+            base = null;
+        }
+        return base;
+    }
+}
