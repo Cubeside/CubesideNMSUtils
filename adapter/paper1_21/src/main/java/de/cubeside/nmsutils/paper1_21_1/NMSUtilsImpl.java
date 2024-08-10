839a15171fcc6fcaca622c11a8409fd4bef0c5b5
Brokkonaut <hannos17@gmx.de>
2024-08-10 04:24:56 +0200

    1.21.1

---
+++ adapter/paper1_21/src/main/java/de/cubeside/nmsutils/paper1_21_1/NMSUtilsImpl.java
@@ -0,0 +1,9 @@
+package de.cubeside.nmsutils.paper1_21_1;
+
+import org.bukkit.plugin.Plugin;
+
+public class NMSUtilsImpl extends de.cubeside.nmsutils.paper1_21.NMSUtilsImpl {
+    public NMSUtilsImpl(Plugin plugin) {
+        super(plugin);
+    }
+}
