e8c098fd439ad753c2334caae1b1dd8810cd4aff
Brokkonaut <hannos17@gmx.de>
2025-04-12 23:47:46 +0200

    utf-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.16" apply false
 }
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    tasks.withType<JavaExec> {
+        systemProperty("file.encoding", "UTF-8")
+    }
+}
