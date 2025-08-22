89b7febf8ba6ba01491220252f158e1abbf9b563
Brokkonaut <hannos17@gmx.de>
2025-08-22 06:49:05 +0200

    update paper userdev dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    maven("https://repo.papermc.io/repository/maven-public/")
+  }
+}
+
 // rootProject.name = "nmsutils_parent"
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
